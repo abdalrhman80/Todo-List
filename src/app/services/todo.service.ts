de790b13894e694b00dbe34ed203681ff3beb740
Abdalrhman Gamal <abdalrhmangamel681@gmail.com>
2024-03-02 05:17:25 +0200

    Modified Service

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -6,27 +6,27 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class TodoService {
-  baseUrl: string = 'https://todos.routemisr.com/api/v1/todos'
+  baseUrl: string = 'https://todos.routemisr.com/api/v1/'
   constructor(private _HttpClient: HttpClient) { }
 
   getApiKey(): Observable<any> {
-    return this._HttpClient.get('${baseUrl}getApiKey',)
+    return this._HttpClient.get(`${this.baseUrl}getApiKey`)
   }
 
   getAllTodoS(apiKey: string): Observable<any> {
-    return this._HttpClient.get(`${this.baseUrl}/${apiKey}`)
+    return this._HttpClient.get(`${this.baseUrl}todos/${apiKey}`)
   }
 
   addTodo(todoData: any): Observable<any> {
-    return this._HttpClient.post(`${this.baseUrl}`, todoData)
+    return this._HttpClient.post(`${this.baseUrl}todos`, todoData)
   }
 
   deleteTodo(todoId: any): Observable<any> {
-    return this._HttpClient.delete(`${this.baseUrl}`,
+    return this._HttpClient.delete(`${this.baseUrl}todos/`,
       { body: { todoId: todoId } })
   }
 
   completedTodo(todoId: any): Observable<any> {
-    return this._HttpClient.put(`${this.baseUrl}`, todoId)
+    return this._HttpClient.put(`${this.baseUrl}todos`, todoId)
   }
 }
